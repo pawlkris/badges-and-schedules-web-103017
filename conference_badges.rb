c8bd21c53bf626b7bf52ecf158f92afce6757bac
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-19 20:07:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(names)
+  return names.collect { |name| badge_maker(name)}
+end
+
+def assign_rooms(names)
+  return names.enum_with_index.map { |name, i| "Hello, #{name}! You'll be assigned to room #{i}!" }
+end
+
+def printer
+  puts(batch_badge_creator(names))
+  puts(assign_rooms(names))
+end
